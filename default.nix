e8a5194742f41ce4109b05098a2859e8052ad1c1
chessai <chessai@users.noreply.github.com>
2023-04-19 13:13:03 -0500

    cleanups (#3)
    * hack around cabal2nix behaviour
    
    * remove commented-out code

--- default.nix
+++ default.nix
@@ -1,9 +1,12 @@
-{
-  compiler ? "ghc94",
-  pkgs ? import (builtins.fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive.3364b5b117f65fe1ce65a3cdd5612a078a3b31e3.tar.gz";
+let
+  pkgsSrc = builtins.fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/3364b5b117f65fe1ce65a3cdd5612a078a3b31e3.tar.gz";
     sha256 = "sha256-Bs6/5LpvYp379qVqGt9mXxxx9GSE789k3oFc+OAL07M=";
-  }) {
+  };
+in
+{
+  compiler ? "ghc944",
+  pkgs ? import pkgsSrc {
     config = {
       allowBroken = false;
       allowUnfree = true;
@@ -26,15 +29,20 @@ let
     sha256 = "04n9chlpbifgc5pa3zx6ff3rji9am6msrbn1z3x1iinjz2xjfp4p";
   }) {};
 
+  lib = pkgs.lib;
+
   hsPkgs = pkgs.haskell.packages.${compiler};
   hsLib = pkgs.haskell.lib;
 
-  libbase64-bindings = hsPkgs.developPackage {
+  # We need this because cabal2nix is a goofy goober
+  removeHsBase64 = lib.lists.remove hsPkgs.base64;
+
+  libbase64-bindings = (hsPkgs.developPackage {
     name = "libbase64-bindings";
     root = nix-gitignore.gitignoreSource ./.;
 
     modifier = drv: hsLib.overrideCabal drv (old: {
-      librarySystemDepends = (old.librarySystemDepends or []) ++ [pkgs.libbase64];
+      librarySystemDepends = removeHsBase64 (old.librarySystemDepends or []) ++ [pkgs.libbase64];
 
       configureFlags = (old.configureFlags or []) ++ [
         "--extra-lib-dirs=${pkgs.libbase64}/lib"
@@ -43,9 +51,12 @@ let
     });
 
     inherit returnShellEnv;
-  };
+  }).overrideAttrs (old: {
+    buildInputs = removeHsBase64 (old.buildInputs or []);
+  });
 in
 {
+  inherit pkgs;
   inherit (pkgs) libbase64;
   inherit libbase64-bindings;
 }
